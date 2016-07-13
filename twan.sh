f602b9c105cea0c8ed40d804d2fa70849172e0d1
David J Patrick <davamundo@gmail.com>
2016-07-12 21:58:03 -0400

    changed tmp file location to /tmp/

--- twan.sh
+++ twan.sh
@@ -6,15 +6,14 @@
 # Distributed under terms of the MIT license.
 #
 
-TMP_LOC="~/tmp"
 ID=$*
 echo $ID
 if [[ "$ID" != [0-9]* ]]; then 
     echo "  ERROR: twan command must be followed by a single task ID"
     exit 1
 fi
-vi $TMP_LOC/tw-annot-$ID.tmp
-task $ID annotate "`cat $TMP_LOC/tw-annot-$ID.tmp`"
-rm $TMP_LOC/tw-annot-$ID.tmp
+vi /tmp/tw-annot-$ID.tmp
+task $ID annotate "`cat /tmp/tw-annot-$ID.tmp`"
+rm /tmp/tw-annot-$ID.tmp
 exit 0
 
